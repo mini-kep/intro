483f89ea77b1fa65be7144628a130d292711f013
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2017-10-17 17:29:42 +0300

    moved code examples

--- util/fx_brent_seaborn_chart_sample.py
+++
@@ -1,2 +0,0 @@
-# TODO 2: add code for same chart as <https://camo.githubusercontent.com/cce545fef204c89f61093e2246d2c489177a39e1/687474703a2f2f6461746163686172742e63632f696d616765732f7275625f6f696c2e706e67
-# based on <https://github.com/epogrebnyak/data-fx-oil/blob/master/plotting.py#L12-L64>
